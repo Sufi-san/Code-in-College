6a4b9334a2cbf4d85f55b3fd7e82202e695e228e
Sufiyan <sufy_456@hotmail.com>
2023-01-27 16:31:14 +0530

    Made Python Folder and added 2 Programs

---
+++ Programs in College (GH)/Python/Program1.py
@@ -0,0 +1,12 @@
+i = 7
+c = 24+8j
+f = 701
+s = 'HELLO EVERYONE!!\nThis is Sufiyan\'s python programming..'
+# NOTE: boolean has truth values that are case-sensitive Ex: True (T is caps!)
+b = True
+print("the value of c is:", c, '\nits type is:', type(i)) 
+print("the value of c is:", f, '\nits type is:', type(f))
+print("the value of c is:", c, '\nits type is:', type(c))
+print("the value of c is:", s, '\nits type is:', type(s))
+print("the value of c is:", b, '\nits type is:', type(b))
+print('NOTE: boolean has truth values that are case sensitive Ex: True (T is caps!)')
